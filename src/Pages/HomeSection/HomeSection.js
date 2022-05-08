c6aea34392cbdd259f585a21a308803365344295
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-08 23:37:27 +0600

    new code

--- src/Pages/HomeSection/HomeSection.js
+++ src/Pages/HomeSection/HomeSection.js
@@ -1,26 +1,26 @@
-import React from "react";
-import useSection from "../../hooks/useSection";
-import "./HomeSection.css";
+// import React from "react";
+// import useSection from "../../hooks/useSection";
+// import "./HomeSection.css";
 
-const HomeSection = () => {
-  const [sections,] = useSection([]);
-  return (
-    <div className="text-center">
-      {/* section one */}
+// const HomeSection = () => {
+//   const [sections,] = useSection([]);
+//   return (
+//     <div className="text-center">
+//       {/* section one */}
    
-      {/* section two */}
-      <h1 className="bg-darking pt-5 feedback">Our Specialties</h1>
-      <div className=" d-flex bg-darking text-dark row justify-content-center align-items-start mx-auto py-5 text-center g-4 ">
-        {sections.slice(0, 3).map((section) => (
-          <div key={section._id} className="col-lg-3 client-style m-2">
-            <img className="w-75 mb-3 client-img" src={section.img} alt="" />
-            <h3 className="my-3 client-name">{section.name}</h3>
-            <p>{section.body}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+//       {/* section two */}
+//       <h1 className="bg-darking pt-5 feedback">Our Specialties</h1>
+//       <div className=" d-flex bg-darking text-dark row justify-content-center align-items-start mx-auto py-5 text-center g-4 ">
+//         {sections.slice(0, 3).map((section) => (
+//           <div key={section._id} className="col-lg-3 client-style m-2">
+//             <img className="w-75 mb-3 client-img" src={section.img} alt="" />
+//             <h3 className="my-3 client-name">{section.name}</h3>
+//             <p>{section.body}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default HomeSection;
+// export default HomeSection;
