aacce59830e479f3b9d924f33217d02292a17fd8
Masum-Arifin <you@example.com>
2023-01-03 19:49:45 +0600

    spiner added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import{useEffect, useState} from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 import { Link } from 'react-router-dom';
 import './Home.css'
 import Product from '../HomeData/Product';
@@ -8,14 +10,34 @@ import HomeSection from '../HomeSection/HomeSection';
 
 const Home = () => {
     
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000)
+    }, [])
     return (
         <div className='text-center '>
+        {
+            loading ?
+            <ClipLoader
+            color={'4fe514'}
+            loading={loading}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+             :
+             
              <div className='home-container text-center pb-4'>
                 <h5 className='text-mine fw-bolder '>Distribution, Storage, Warehousing & Fulfillment</h5>
                 <Link className='see-link mb-5' to="/management">See More</Link>
             </div>
+            }
             <Product></Product>
             <HomeSection></HomeSection>
+        
         </div>
     );
 };
